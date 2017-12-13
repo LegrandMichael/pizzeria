3011b30d4d4f8226e5c890d1d2cbd126ddf339fc
LegrandMichael <michael.c.legrand@gmail.com>
2017-12-13 15:10:20 +0100

    Generate Receipts entity

---
+++ src/AppBundle/Entity/Receipts.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Receipts
+ *
+ * @ORM\Table(name="receipts")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\ReceiptsRepository")
+ */
+class Receipts
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255, unique=true)
+     */
+    private $title;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return Receipts
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+}
+
