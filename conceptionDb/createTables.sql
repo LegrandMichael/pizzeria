71730a8d61333e72054010a59f80d8f10e86d277
LegrandMichael <michael.c.legrand@gmail.com>
2017-12-18 13:50:20 +0100

    Script SQL create Tables

--- conceptionDb/createTables.sql
+++ conceptionDb/createTables.sql
@@ -2,11 +2,12 @@
 #        Script MySQL.
 #------------------------------------------------------------
 
+
 #------------------------------------------------------------
-# Table: Users
+# Table: User
 #------------------------------------------------------------
 
-CREATE TABLE Users(
+CREATE TABLE User(
         id          int (11) Auto_increment  NOT NULL ,
         firstName   Varchar (255) NOT NULL ,
         lastName    Varchar (255) NOT NULL ,
@@ -16,45 +17,48 @@ CREATE TABLE Users(
         birthdate   Varchar (10) NOT NULL ,
         phoneNumber Varchar (9) NOT NULL ,
         address     Varchar (255) NOT NULL ,
+        zipCode     Int NOT NULL ,
+        city        Varchar (255) NOT NULL ,
         PRIMARY KEY (id ) ,
         UNIQUE (nickname )
 )ENGINE=InnoDB;
 
 
 #------------------------------------------------------------
-# Table: Orders
+# Table: Purchase
 #------------------------------------------------------------
 
-CREATE TABLE Orders(
-        id          int (11) Auto_increment  NOT NULL ,
-        id_Users    Int NOT NULL ,
-        id_Products Int ,
+CREATE TABLE Purchase(
+        id         int (11) Auto_increment  NOT NULL ,
+        quantity   Int NOT NULL ,
+        id_User    Int NOT NULL ,
+        id_Product Int ,
         PRIMARY KEY (id )
 )ENGINE=InnoDB;
 
 
 #------------------------------------------------------------
-# Table: Products
+# Table: Product
 #------------------------------------------------------------
 
-CREATE TABLE Products(
-        id          int (11) Auto_increment  NOT NULL ,
-        name        Varchar (30) ,
-        price       Float ,
-        id_Receipts Int ,
+CREATE TABLE Product(
+        id        int (11) Auto_increment  NOT NULL ,
+        name      Varchar (30) ,
+        price     Float ,
+        id_Recipe Int ,
         PRIMARY KEY (id )
 )ENGINE=InnoDB;
 
 
 #------------------------------------------------------------
-# Table: Receipts
+# Table: Recipe
 #------------------------------------------------------------
 
-CREATE TABLE Receipts(
-        id          int (11) Auto_increment  NOT NULL ,
-        title       Varchar (25) NOT NULL ,
-        id_Stock    Int ,
-        id_Products Int ,
+CREATE TABLE Recipe(
+        id         int (11) Auto_increment  NOT NULL ,
+        title      Varchar (25) NOT NULL ,
+        id_Stock   Int ,
+        id_Product Int ,
         PRIMARY KEY (id ) ,
         UNIQUE (title )
 )ENGINE=InnoDB;
@@ -83,9 +87,9 @@ CREATE TABLE Unit(
         PRIMARY KEY (id )
 )ENGINE=InnoDB;
 
-ALTER TABLE Orders ADD CONSTRAINT FK_Orders_id_Users FOREIGN KEY (id_Users) REFERENCES Users(id);
-ALTER TABLE Orders ADD CONSTRAINT FK_Orders_id_Products FOREIGN KEY (id_Products) REFERENCES Products(id);
-ALTER TABLE Products ADD CONSTRAINT FK_Products_id_Receipts FOREIGN KEY (id_Receipts) REFERENCES Receipts(id);
-ALTER TABLE Receipts ADD CONSTRAINT FK_Receipts_id_Stock FOREIGN KEY (id_Stock) REFERENCES Stock(id);
-ALTER TABLE Receipts ADD CONSTRAINT FK_Receipts_id_Products FOREIGN KEY (id_Products) REFERENCES Products(id);
+ALTER TABLE Purchase ADD CONSTRAINT FK_Purchase_id_User FOREIGN KEY (id_User) REFERENCES User(id);
+ALTER TABLE Purchase ADD CONSTRAINT FK_Purchase_id_Product FOREIGN KEY (id_Product) REFERENCES Product(id);
+ALTER TABLE Product ADD CONSTRAINT FK_Product_id_Recipe FOREIGN KEY (id_Recipe) REFERENCES Recipe(id);
+ALTER TABLE Recipe ADD CONSTRAINT FK_Recipe_id_Stock FOREIGN KEY (id_Stock) REFERENCES Stock(id);
+ALTER TABLE Recipe ADD CONSTRAINT FK_Recipe_id_Product FOREIGN KEY (id_Product) REFERENCES Product(id);
 ALTER TABLE Stock ADD CONSTRAINT FK_Stock_id_Unit FOREIGN KEY (id_Unit) REFERENCES Unit(id);
